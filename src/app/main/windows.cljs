f17149b573e16a395b1eeaf2d2161cd858168302
threddast <giorgiserik@gmail.com>
2022-07-17 21:27:51 +0200

    src/app/main/windows.cljs

--- src/app/main/windows.cljs
+++ src/app/main/windows.cljs
@@ -23,15 +23,12 @@
 (defn go-to-url
   [{:keys [word-or-phrase target-lang native-lang]}]
   (.loadURL ^js (.-webContents ^BrowserView @t-win)
-            (if (and word-or-phrase target-lang native-lang)
-              ;; (str "https://editorportal.oslomet.no/api/v1/findwords?"
-              ;;      "searchWord=" word-or-phrase
-              ;;      "&lang=bokm%C3%A5l-engelsk&page=1&selectLang=bokm%C3%A5l")
-              (clj->js {"https://translate.google.com", :extraHeaders "Origin: https://lexin.oslomet.no"})
+           ; (if (and word-or-phrase target-lang native-lang)
+            (str "https://ordbokene.no/bm/search?q=" word-or-phrase "&scope=ei&perPage=20")))
               ;; str "https://translate.google.com"
               ;; "?sl=" target-lang "&tl=" native-lang
               ;; "&text=" word-or-phrase "&op=translate"
-              "https://translate.google.com")))
+            ;  "https://translate.google.com")))
 
 (defn t-win-init
   "Used for mini google translate window."
